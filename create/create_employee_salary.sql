afad3ba4189503c0bef9f38ad5656e051d05bab9
mulakalaashritha <153161182+mulakalaashritha@users.noreply.github.com>
2025-10-17 15:48:14 +0530

    Update create_employee_salary.sql

--- create/create_employee_salary.sql
+++ create/create_employee_salary.sql
@@ -5,7 +5,11 @@ CREATE TABLE  employee_salary (
     bonus NUMERIC(10,2),
     total_salary NUMERIC(10,2) 
 );
-insert into employee_salary(123,1000,50000,20000,70000);
-insert into employee_salary(456,1100,50000,10000,60000);
-insert into employee_salary(789,1200,50000,15000,65000);
-select * from employee_salary;
+INSERT INTO employee_salary (salary_id, employee_id, basic_pay, bonus, total_salary)
+VALUES (123, 1000, 50000, 20000, 70000);
+
+INSERT INTO employee_salary (salary_id, employee_id, basic_pay, bonus, total_salary)
+VALUES (456, 1100, 50000, 10000, 60000);
+
+INSERT INTO employee_salary (salary_id, employee_id, basic_pay, bonus, total_salary)
+VALUES (789, 1200, 50000, 15000, 65000);
