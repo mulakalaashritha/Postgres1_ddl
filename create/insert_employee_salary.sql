7e437976de6b47ba0041b5fc1f10f5195153a969
mulakalaashritha <153161182+mulakalaashritha@users.noreply.github.com>
2025-10-17 16:02:10 +0530

    Create insert_employee_salary.sql

---
+++ create/insert_employee_salary.sql
@@ -0,0 +1,8 @@
+INSERT INTO employee_salary (salary_id, employee_id, basic_pay, bonus, total_salary)
+VALUES (123, 1000, 50000, 20000, 70000);
+
+INSERT INTO employee_salary (salary_id, employee_id, basic_pay, bonus, total_salary)
+VALUES (456, 1100, 50000, 10000, 60000);
+
+INSERT INTO employee_salary (salary_id, employee_id, basic_pay, bonus, total_salary)
+VALUES (789, 1200, 50000, 15000, 65000);
