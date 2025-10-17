5f0ec68f9a1348359c8d36b00e8c714863f67c44
mulakalaashritha <153161182+mulakalaashritha@users.noreply.github.com>
2025-10-17 16:01:25 +0530

    Delete create/ddl/insert_employee_salary.sql

--- create/ddl/insert_employee_salary.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO employee_salary (salary_id, employee_id, basic_pay, bonus, total_salary)
-VALUES (123, 1000, 50000, 20000, 70000);
-
-INSERT INTO employee_salary (salary_id, employee_id, basic_pay, bonus, total_salary)
-VALUES (456, 1100, 50000, 10000, 60000);
-
-INSERT INTO employee_salary (salary_id, employee_id, basic_pay, bonus, total_salary)
-VALUES (789, 1200, 50000, 15000, 65000);
